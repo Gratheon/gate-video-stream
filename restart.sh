b41b1bccdcc73f69522fd69a4b1e0a08887e969a
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-05-02 00:01:08 +0300

    create tmp folder

--- restart.sh
+++ restart.sh
@@ -3,6 +3,9 @@ COMPOSE_PROJECT_NAME=gratheon docker-compose -f docker-compose.prod.yml down
 
 chown www:www-data -R /www/gate-video-stream
 
+# tmp folder for videos
+sudo -u www bash -c 'mkdir -p /www/gate-video-stream/tmp'
+
 # installing dependencies is faster on host than in the image
 sudo -u www bash -c 'cd /www/gate-video-stream/ && source ~/.nvm/nvm.sh && nvm use && npm i && npm run build'
 
