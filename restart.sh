06f97fc3a115b8ecd61571e73618337b4883b9a6
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-05-27 01:36:18 +0300

    fix cleanup

--- restart.sh
+++ restart.sh
@@ -5,7 +5,7 @@ chown www:www-data -R /www/gate-video-stream
 
 # tmp folder for videos
 sudo -u www bash -c 'mkdir -p /www/gate-video-stream/tmp'
-sudo -u www bash -c 'rm -rf /www/gate-video-stream/app'
+rm -rf /www/gate-video-stream/app
 
 # installing dependencies is faster on host than in the image
 sudo -u www bash -c 'cd /www/gate-video-stream/ && source ~/.nvm/nvm.sh && nvm use && npm i && npm run build'
