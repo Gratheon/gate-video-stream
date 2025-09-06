aac85bcd0b2a3960d6428abf58353e1ac2fd713b
Artjom Kurapov <artkurapov@gmail.com>
2025-09-07 01:17:58 +0300

    fix restarting

--- restart.sh
+++ restart.sh
@@ -1,13 +1,11 @@
 cd /www/gate-video-stream/
 COMPOSE_PROJECT_NAME=gratheon docker-compose -f docker-compose.prod.yml down
 
-chown www:www-data -R /www/gate-video-stream
-
 # tmp folder for videos
-sudo -u www bash -c 'mkdir -p /www/gate-video-stream/tmp'
+mkdir -p /www/gate-video-stream/tmp
 rm -rf /www/gate-video-stream/app
 
 # installing dependencies is faster on host than in the image
-sudo -u www bash -c 'cd /www/gate-video-stream/ && source ~/.nvm/nvm.sh && nvm use && npm i && npm run build'
+cd /www/gate-video-stream/ && source ~/.nvm/nvm.sh && nvm use && npm i && npm run build
 
 COMPOSE_PROJECT_NAME=gratheon docker-compose -f docker-compose.prod.yml up -d --build
